e98af4b220f80be235fcf4a6eb4ebd725c94f686
Ueder Carlos Costa Caetano <uedercosta@gmail.com>
2026-06-10 20:05:17 -0300

    Commit inicial - Criação do projeto

---
+++ cursos/src/test/java/br/com/desafio/cursos/CursosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafio.cursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
